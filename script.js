4a1037ece1b1604183983b3e8fd28e1ac12f52b2
Benjamin-Harris1 <beha1503@gmail.com>
2023-03-29 17:46:20 +0200

    general changes

--- script.js
+++ script.js
@@ -4,50 +4,42 @@ window.addEventListener("load", startApp);
 
 async function startApp() {
   const character = await getCharacter();
-
   character.forEach(showCharacter);
 }
 
 async function getCharacter() {
-  const response = await fetch("data/data.json");
+  const response = await fetch("https://cederdorff.github.io/dat-js/05-data/southpark.json");
   const data = await response.json();
   return data;
 }
 
-function showCharacter(person) {
+function showCharacter(character) {
   const HTML =
     /*html*/
     `
     <article class="grid-item">
-    <img src="${person.image}"">
-    <h1><span>${person.name}</span></h1>
-    <p><strong>Aka: </strong><span>${person.nickname}</span></spa></p>
+    <img src="${character.image}"">
+    <h1><span>${character.name}</span></h1>
+    <p><strong>Aka: </strong><span>${character.nickname}</span></spa></p>
     </article>
       `;
   document.querySelector("#characters").insertAdjacentHTML("beforeend", HTML);
   document.querySelector("#characters article:last-child").addEventListener("click", characterClicked);
 
   function characterClicked() {
-    document.querySelector("#dialog-image").src = person.image;
-    document.querySelector("#dialog-name").textContent = person.name;
-    document.querySelector("#dialog-nickname").textContent = person.nickname;
-    document.querySelector("#dialog-occupation").textContent = person.occupation;
-    document.querySelector("#dialog-age").textContent = person.age;
-    document.querySelector("#dialog-voicedby").textContent = person.voicedBy;
-    document.querySelector("#dialog-gender").textContent = person.gender;
-    document.querySelector("#dialog-religion").textContent = person.religion;
-    document.querySelector("#dialog-hair").textContent = person.hairColor;
-    document.querySelector("#dialog-schoolgrade").textContent = person.schoolGrade;
-    document.querySelector("#dialog-episodes").textContent = person.episodes;
-    document.querySelector("#dialog-appearances").textContent = person.appearances;
-    document.querySelector("#dialog-firstappearance").textContent = person.firstAppearance;
+    document.querySelector("#dialog-image").src = character.image;
+    document.querySelector("#dialog-name").textContent = character.name;
+    document.querySelector("#dialog-nickname").textContent = character.nickname;
+    document.querySelector("#dialog-occupation").textContent = character.occupation;
+    document.querySelector("#dialog-age").textContent = character.age;
+    document.querySelector("#dialog-voicedby").textContent = character.voicedBy;
+    document.querySelector("#dialog-gender").textContent = character.gender;
+    document.querySelector("#dialog-religion").textContent = character.religion;
+    document.querySelector("#dialog-hair").textContent = character.hairColor;
+    document.querySelector("#dialog-schoolgrade").textContent = character.schoolGrade;
+    document.querySelector("#dialog-episodes").textContent = character.episodes;
+    document.querySelector("#dialog-appearances").textContent = character.appearances;
+    document.querySelector("#dialog-firstappearance").textContent = character.firstAppearance;
     document.querySelector("#dialog").showModal();
   }
 }
-
-function showCharacterModal(list) {
-  showCharacter(person);
-}
-function showDialog(person) {}
-
-function closeDialog() {}
